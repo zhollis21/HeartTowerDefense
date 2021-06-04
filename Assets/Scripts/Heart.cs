9e0d3b62d8149eaabb730f9101e2eca2cce8719f
Zach Hollis <43797367+zhollis21@users.noreply.github.com>
2021-06-03 23:53:03 -0400

    Initial Project Creation and some spawning work

---
+++ Assets/Scripts/Heart.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heart : MonoBehaviour
+{
+    public enum HeartType { Red, Blue, Green, Yellow }
+
+    public HeartType Type;
+    public int HitPoints;
+    public float Speed;
+
+    private List<Vector3> _mapPoints;
+    private int _pointIndex = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _mapPoints = GameManager.Instance.CurrentMap.PathPoints;
+        transform.position = _mapPoints[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _mapPoints[_pointIndex], Time.deltaTime * Speed * 2);
+
+        if (transform.position == _mapPoints[_pointIndex])
+        {
+            _pointIndex++;
+
+            if (_pointIndex == _mapPoints.Count)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
