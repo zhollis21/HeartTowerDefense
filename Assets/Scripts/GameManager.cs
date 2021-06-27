76663a0b6fd021d53e896ba0609912c50af7f9d5
Zach Hollis <43797367+zhollis21@users.noreply.github.com>
2021-06-27 17:27:59 -0400

    Fixing conflict

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,35 @@
+<<<<<<< HEAD
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int Round;
+    public int Lives;
+    public int Money;
+    public Map CurrentMap;
+    public Heart RedHeart;
+    public Heart BlueHeart;
+    public static GameManager Instance;
+
+    public const int MAX_ROUND = 5;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+=======
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -28,6 +60,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+>>>>>>> d351a87f7625c2e4dce441c02b2c330d4be84cb7
         if (Round < MAX_ROUND && !RoundManager.Instance.IsRoundPlaying())
         {
             Round++;
